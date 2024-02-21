e9e2f5bed08702281b32114bfe13a392356ec756
epenzina95 <34599062+epenzina95@users.noreply.github.com>
2024-02-21 20:16:32 +0100

    Add menu to the main app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit
 
 streamlit.title("My parents' new healthy dinner")
+
+streamlit.header("Breakfast menu")
+streamlit.text("Omega 3 and blueberry oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-boiled three-ranged egg")
+
 print('Hello World')
