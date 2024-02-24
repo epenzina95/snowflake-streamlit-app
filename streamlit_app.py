50ad936e8ce4a29b9a181b8c079ee0dce1ad65d3
epenzina95 <34599062+epenzina95@users.noreply.github.com>
2024-02-24 10:25:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,14 +39,18 @@ def get_fruit_load_list():
     my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
     return my_cur.fetchall()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-
 if streamlit.button('Get Fruit Load List'):
   streamlit.header("The fuir load list contains:")
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
-fruit_add = streamlit.text_input('What fuit would you like to add?', 'jackfruit')
-streamlit.write('Thanks for adding', fruit_add)
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values('from streamlit')")
+    return f'Thanks for adding {new_fruit}'
+
+fruit_add = streamlit.text_input('What fuit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  streamlit.text(insert_row_snowflake(fruit_add))
